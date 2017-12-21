0f35edff2d86f46df6e6b1a1dfd8b4c9e6c2169a
mrfarhadbd <ahmedfarhad1000@gmail.com>
2017-12-21 07:42:03 -0800

    template

--- s.js
+++ s.js
@@ -2,3 +2,10 @@ var $ = jQuery;
 $('#list').on('click',function(){
 	$('ul').toggle();
 })
+
+$('#color').on('click', function(){
+	var random=parseInt(Math.random() *  255)
+	var random1=parseInt(Math.random() * 255)
+	var random2=parseInt(Math.random() * 255)
+  $('body').css('background', `rgb(${random}, ${random1}, ${random2})`)
+})
